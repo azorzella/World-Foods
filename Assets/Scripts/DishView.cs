ea3c03354e6487f890fd95ef2e13915ec376152d
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-05 00:32:29 -0800

    Cache was not getting updated

--- Assets/Scripts/DishView.cs
+++ Assets/Scripts/DishView.cs
@@ -81,5 +81,7 @@ public class DishView : MonoBehaviour {
         canvasGroup.alpha = visible ? 1 : 0;
         canvasGroup.interactable = visible;
         canvasGroup.blocksRaycasts = visible;
+        
+        this.visible = visible;
     }
 }
