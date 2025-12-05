b7b42188534d60142e0120b5355ce4a0a36d1a5a
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-05 01:12:35 -0800

    Removed debug log displaying the name of the clicked country. For some reason, it's hard to get a simple click to behave. About to try getting the device

--- Assets/Scripts/Interaction.cs
+++ Assets/Scripts/Interaction.cs
@@ -15,20 +15,15 @@ public class Interaction : MonoBehaviour {
     }
 
     public void OnClick(InputAction.CallbackContext context) {
-        Debug.Log(context.ReadValue<Vector2>());
-        
-        if (!DishView.i.IsVisible()) { // && Mouse.current.leftButton.wasPressedThisFrame) {
-            // Ray ray = camera.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (!DishView.i.IsVisible()) {
             Ray ray = camera.ScreenPointToRay(context.ReadValue<Vector2>());
 
             if (Physics.Raycast(ray, out var hit)) {
                 string isoCode = hit.transform.name;
-                Debug.Log(isoCode);
-                
                 DishView.i.OpenDisplaying(vis.GetLoggedDishesFromCountry(isoCode));
             }
         }
-    }
+    }   
 
     public void OnDrag(InputAction.CallbackContext context) {
         if (!DishView.i.IsVisible()) {
