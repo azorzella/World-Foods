bffe358146c8ec9280ea282edeab10072e1f04c2
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-06 14:32:55 -0800

    Settings up ratings panel

--- Assets/Scripts/DishEntry.cs
+++ Assets/Scripts/DishEntry.cs
@@ -1,26 +1,13 @@
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class DishEntry : MonoBehaviour, IPointerClickHandler {
+public class DishEntry : MonoBehaviour {
     TextMeshProUGUI textComponent;
     Dish dishCache;
-
-    DishView dishView;
     
-    public void Initialize(DishView dishView, Dish dish) {
-        this.dishView = dishView;
+    public void Initialize(Dish dish) {
         dishCache = dish;
-        
         textComponent = GetComponentInChildren<TextMeshProUGUI>();
         textComponent.text = dishCache.GetName();
-    }
-    
-    public void OnPointerClick(PointerEventData eventData) {
-        dishView.ShowRatingPanelFor(dishCache);
-    }
-
-    public void HideRatingPanel_BucketBrigade() {
-        dishView.HideRatingPanel();
     }
 }
