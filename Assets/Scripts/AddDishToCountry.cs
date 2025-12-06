675fe90af9b3605f3ad44ad24731d46997cf184a
Chanel <152814087+chanelko@users.noreply.github.com>
2025-12-06 15:42:25 -0800

    Add AddDishToCatalogue function

---
+++ Assets/Scripts/AddDishToCountry.cs
@@ -0,0 +1,9 @@
+namespace DefaultNamespace;
+
+public class AddDishToCountry : MonoBehavior
+{
+    TextMeshProUGUI Button;
+    // On user click, add dish to user's list of dishes for that country
+    
+    
+}
