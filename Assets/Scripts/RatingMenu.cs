e0d2154235be9a93e359f2813d4a52842cf7731d
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-06 15:22:32 -0800

    Ratings work

--- Assets/Scripts/RatingMenu.cs
+++ Assets/Scripts/RatingMenu.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using Codice.Client.BaseCommands.CheckIn.Progress;
+using TMPro;
 using UnityEngine;
 
 public interface RatingMenuListener {
@@ -9,8 +11,30 @@ public interface RatingMenuListener {
 public class RatingMenu : MonoBehaviour {
     Dish currentDish;
 
+    TextMeshProUGUI text;
+    CanvasGroup canvasGroup;
+
     readonly List<RatingMenuListener> listeners = new();
 
+    void Awake() {
+        text = GetComponentInChildren<TextMeshProUGUI>();
+        canvasGroup = GetComponent<CanvasGroup>();
+    }
+
+    void SetCanvasGroupVisible(bool visible) {
+        canvasGroup.alpha = visible ? 1 : 0;
+        canvasGroup.interactable = visible;
+        canvasGroup.blocksRaycasts = visible;
+    }
+    
+    public void SetDish(Dish dish) {
+        currentDish = dish;
+        text.text = $"Rate {dish.GetName()}";
+        
+        NotifyListeners();
+        Show();
+    }
+    
     public void RegisterListener(RatingMenuListener listener) {
         listeners.Add(listener);
 
@@ -29,5 +53,13 @@ public class RatingMenu : MonoBehaviour {
         currentDish.SetRating(newRating);
         
         NotifyListeners();
+    }
+
+    void Show() {
+        SetCanvasGroupVisible(true);
+    }
+
+    public void Hide() {
+        SetCanvasGroupVisible(false);
     }
 }
