913df296f847fcee847d32098908a9ccc6ea94de
chanel-koh <chanelkoh04@gmail.com>
2025-12-06 23:27:25 -0800

    Add functionality for AddDishToCountry
    # Conflicts:
    #	Assets/Scripts/WorldMapVisualization.cs

--- Assets/Scripts/AddDishToCountry.cs
+++ Assets/Scripts/AddDishToCountry.cs
@@ -1,6 +1,4 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
@@ -10,26 +8,14 @@ public class AddDishToCountry : MonoBehaviour
     TextMeshProUGUI Button;
     
     readonly List<Dish> filteredResults = new();
-    readonly List<string> countryNames = new();
     
     Dish dish;
-    int country_index = 0;
     
-    public TMP_InputField dishNameInput;
     public TMP_Dropdown dropdown;
-    public TMP_Dropdown countryDropdown;
-
-    private void Start()
-    {
-        countryDropdown.gameObject.SetActive(false);
-    }
-
     public void EntryFilter(string entry)
     {
-        countryDropdown.gameObject.SetActive(false);
-        
         filteredResults.Clear();
-        
+
         if (entry.Length < 3)
         {
             return;
@@ -44,19 +30,8 @@ public class AddDishToCountry : MonoBehaviour
                 filteredResults.Add(d);
                 filteredDishNames.Add(d.GetName());
             }
+        }
 
-        }
-        
-        if (filteredResults.Count == 0)
-        {
-            countryDropdown.gameObject.SetActive(true);
-            
-            foreach (var pair in DishCatalogue.isoCodes)
-            {
-                countryNames.Add(pair.Key);
-            }
-            countryDropdown.AddOptions(countryNames);;
-        }
         dropdown.ClearOptions();
         dropdown.AddOptions(filteredDishNames);
     }
@@ -65,23 +40,9 @@ public class AddDishToCountry : MonoBehaviour
     {
         dish = filteredResults[index];
     }
-
-    public void countrySelected(int index)
-    {
-        country_index = index;
-    }
     
     public void LogDish()
     {
-        if (country_index == 0)
-        {
-            FindFirstObjectByType<WorldMapVisualization>().LogDish(dish);
-        }
-        else
-        {
-            Dish d = new(dishNameInput.text, DishCatalogue.isoCodes.ElementAt(country_index).Value);
-            FindFirstObjectByType<WorldMapVisualization>().LogDish(d);
-        }
-        
+        FindFirstObjectByType<WorldMapVisualization>().LogDish(dish);
     }
 }
