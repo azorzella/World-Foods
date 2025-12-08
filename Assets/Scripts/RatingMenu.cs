e64c74cf401b42b694b4844cce56143909901285
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-07 19:31:42 -0800

    Documented and cleaned up RatingMenu.cs

--- Assets/Scripts/RatingMenu.cs
+++ Assets/Scripts/RatingMenu.cs
@@ -11,16 +11,23 @@ public class RatingMenu : MonoBehaviour {
     readonly List<RatingMenuListener> listeners = new();
 
     void Awake() {
+        CacheComponents();
+    }
+
+    /// <summary>
+    /// Caches the text component found in its children and
+    /// its canvas group
+    /// </summary>
+    void CacheComponents() {
         text = GetComponentInChildren<TextMeshProUGUI>();
         canvasGroup = GetComponent<CanvasGroup>();
     }
-
-    void SetCanvasGroupVisible(bool visible) {
-        canvasGroup.alpha = visible ? 1 : 0;
-        canvasGroup.interactable = visible;
-        canvasGroup.blocksRaycasts = visible;
-    }
     
+    /// <summary>
+    /// Sets the current dish being rated and notifies the rating
+    /// listeners as the current dish was updated
+    /// </summary>
+    /// <param name="dish"></param>
     public void SetDish(Dish dish) {
         currentDish = dish;
         text.text = $"Rate {dish.GetName()}";
@@ -29,6 +36,10 @@ public class RatingMenu : MonoBehaviour {
         Show();
     }
     
+    /// <summary>
+    /// Adds the passed listener to the list of listeners
+    /// </summary>
+    /// <param name="listener"></param>
     public void RegisterListener(RatingMenuListener listener) {
         listeners.Add(listener);
 
@@ -37,22 +48,47 @@ public class RatingMenu : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Notifies all the rating listeners of the current dish's rating
+    /// </summary>
     void NotifyListeners() {
         foreach (var listener in listeners) {
             listener.NotifyRatingChanged(currentDish.GetRating());
         }
     }
 
+    /// <summary>
+    /// Sets the current dish's rating
+    /// </summary>
+    /// <param name="newRating"></param>
     public void SetRating(int newRating) {
         currentDish.SetRating(newRating);
         
         NotifyListeners();
     }
 
+    /// <summary>
+    /// Sets the alpha, interactability, and raycast blocking flag of
+    /// the cached canvas group according to whether it's visible or
+    /// not
+    /// </summary>
+    /// <param name="visible"></param>
+    void SetCanvasGroupVisible(bool visible) {
+        canvasGroup.alpha = visible ? 1 : 0;
+        canvasGroup.interactable = visible;
+        canvasGroup.blocksRaycasts = visible;
+    }
+    
+    /// <summary>
+    /// Opens the menu
+    /// </summary>
     void Show() {
         SetCanvasGroupVisible(true);
     }
 
+    /// <summary>
+    /// Closes the menu
+    /// </summary>
     public void Hide() {
         SetCanvasGroupVisible(false);
     }
