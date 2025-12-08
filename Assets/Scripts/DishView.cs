07cdd3ff8c4ecdab642d6e2baccbc8bf0e865eba
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-07 19:07:57 -0800

    Added documentation and cleaned up DishView.cs

--- Assets/Scripts/DishView.cs
+++ Assets/Scripts/DishView.cs
@@ -1,11 +1,15 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class DishView : MonoBehaviour, MenuListener {
     static DishView _i;
 	
+    /// <summary>
+    /// Returns the static instance of DishView. If the instance
+    /// has not been set, then it instantiates a new instance of
+    /// DishView using the resource loader, caches it, and returns it
+    /// </summary>
     public static DishView i {
         get {
             if (_i == null) {
@@ -19,7 +23,7 @@ public class DishView : MonoBehaviour, MenuListener {
     }
     
     public RectTransform parentEntriesTo;
-    [FormerlySerializedAs("ratingPanelCanvasGroup")] public RatingMenu ratingMenu;
+    public RatingMenu ratingMenu;
     
     readonly List<GameObject> entries = new();
 
@@ -32,20 +36,29 @@ public class DishView : MonoBehaviour, MenuListener {
         FindFirstObjectByType<Menu>().RegisterListener(this);
     }
 
+    /// <summary>
+    /// Caches its canvas group and the vertical layout group found on the entry parent,
+    /// hides itself, and hides the rating panel
+    /// </summary>
     void CacheComponents() {
         canvasGroup = GetComponent<CanvasGroup>();
         layoutGroup = parentEntriesTo.gameObject.GetComponent<VerticalLayoutGroup>();
-        
-        SetSelfVisible(false);
+
+        Close();
         HideRatingPanel();
     }
     
+    /// <summary>
+    /// Shows the menu displaying the passed list of dishes parented to the scroll view,
+    /// resized to fit the dish entries
+    /// </summary>
+    /// <param name="dishes"></param>
     public void OpenDisplaying(List<Dish> dishes) {
         if (dishes.Count <= 0) {
             return;
         }
         
-        Clear();
+        ClearEntries();
 
         GameObject dishEntry = ResourceLoader.LoadObject("DishEntry");
 
@@ -69,31 +82,43 @@ public class DishView : MonoBehaviour, MenuListener {
         SetSelfVisible(true);
     }
     
-    void Clear() {
+    /// <summary>
+    /// Clears the list of entries
+    /// </summary>
+    void ClearEntries() {
         while (entries.Count > 0) {
             Destroy(entries[0]);
             entries.RemoveAt(0);
         }
     }
 
+    /// <summary>
+    /// Hides itself
+    /// </summary>
     public void Close() {
         SetSelfVisible(false);
     }
 
     bool visible;
 
+    /// <summary>
+    /// Returns whether it's currently visible
+    /// </summary>
+    /// <returns></returns>
     public bool IsVisible() {
         return visible;
     }
     
-    void SetCanvasGroupVisible(CanvasGroup group, bool visible) {
-        group.alpha = visible ? 1 : 0;
-        group.interactable = visible;
-        group.blocksRaycasts = visible;
-    }
-
+    /// <summary>
+    /// Updates the parameters of its cached canvas group to either show or hide itself,
+    /// and then automatically hides the ratings panel when it gets closed
+    /// </summary>
+    /// <param name="visible"></param>
     void SetSelfVisible(bool visible) {
-        SetCanvasGroupVisible(canvasGroup, visible);
+        canvasGroup.alpha = visible ? 1 : 0;
+        canvasGroup.interactable = visible;
+        canvasGroup.blocksRaycasts = visible;
+        
         this.visible = visible;
 
         if (!visible) {
@@ -101,14 +126,25 @@ public class DishView : MonoBehaviour, MenuListener {
         }
     }
 
+    /// <summary>
+    /// Shows the ratings panel for the passed dish
+    /// </summary>
+    /// <param name="dish"></param>
     public void ShowRatingPanelFor(Dish dish) {
         ratingMenu.SetDish(dish);
     }
 
+    /// <summary>
+    /// Hides the rating panel
+    /// </summary>
     public void HideRatingPanel() {
         ratingMenu.Hide();
     }
 
+    /// <summary>
+    /// Hides itself if the user clicks another tab besides the map view
+    /// </summary>
+    /// <param name="currentIndex"></param>
     public void NotifyMenuStateChanged(int currentIndex) {
         if (currentIndex > 0) {
             SetSelfVisible(false);
