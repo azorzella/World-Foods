cb447ab21570b8d02540d03a773c8063617e877f
Alex Zorzella <43708160+alexzorzella@users.noreply.github.com>
2025-12-07 18:31:48 -0800

    Refactored and CountryObject.cs

--- Assets/Scripts/CountryObject.cs
+++ Assets/Scripts/CountryObject.cs
@@ -1,23 +1,22 @@
 using System;
 using UnityEngine;
 
-public interface VisListener {
-    void OnValueChanged(float newValue);
-}
-
 public class CountryObject : MonoBehaviour, VisListener {
-    static readonly Color defaultColor = new Color(0.4608599F, 0.7672955F, 0.7201515F);
+    static readonly Color defaultColor = new(0.4608599F, 0.7672955F, 0.7201515F);
     
     string isoCode;
-
-    Renderer renderer;
     
     void Start() {
-        renderer = GetComponent<Renderer>();
+        Initialize();
+    }
+
+    // Caches the country object's ISO code and registers it to the WorldMapVisualization
+    void Initialize() {
         isoCode = gameObject.name.ToUpper();
         transform.root.GetComponent<WorldMapVisualization>().RegisterListener(this, isoCode);
     }
 
+    // Updates the object's color whenever the WorldMapVisualization notifies it of its new gradient value
     public void OnValueChanged(float newValue) {
         LeanTween.cancel(gameObject);
 
